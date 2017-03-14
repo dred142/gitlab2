628a25e3b046baee48527f07b3350325f7f76fcb
kidaww <bulakhvad@gmail.com>
2017-03-14 17:58:17 +0300

    Отрпавил проект в репозиторий

---
+++ ppppl1/ppppl1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ppppl1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работа с Git репозиторием");
+            Console.ReadLine();
+        }
+    }
+}
